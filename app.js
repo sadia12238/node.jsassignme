4e754e68044e69381dc0c5feb2821bc14fbfbf54
sadia12238 <sacdiyacimraan@gmail.com>
2024-11-03 10:31:46 +0300

    Add files via upload

---
+++ app.js
@@ -0,0 +1,16 @@
+const http = require('http');
+const taskRoutes = require('./routes/taskRoutes');
+
+const HOSTNAME = 'localhost'
+const PORT = 9000
+
+const server = http.createServer((req, res) => {
+    if (req.url.startsWith('/tasks')){
+        taskRoutes(req, res)
+    }else{
+        res.writeHead(404, 'Not Found', {'content-type': 'application/json'})
+        res.end(JSON.stringify({
+            message: 'sorry, you got lost!'
+        }))
+    }
+});
